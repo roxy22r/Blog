150c817a93528f51dabea2f0456512ae3a07d60b
roxy22r <raksana280@gmail.com>
2022-08-14 19:51:57 +0200

    RAK first init Commit Includes auto carousle & nav

---
+++ Blog/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SlideShowComponent } from './slide-show/slide-show.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { AboutMeComponent } from './about-me/about-me.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    SlideShowComponent,
+    AboutMeComponent,
+  ],
+  imports: [
+    SlickCarouselModule,
+    BrowserModule,
+    AppRoutingModule,
+      
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ Blog/src/app/slide-show/slide-show.component.ts
@@ -0,0 +1,71 @@
+import { Component, Injectable, Input, NgModule, OnInit } from '@angular/core';
+interface carouselImage{
+  imageSrc: string;
+  imageAlt: string;
+}
+@Component({
+
+  selector: 'app-slide-show',
+  templateUrl: './slide-show.component.html',
+  styleUrls: ['./slide-show.component.css']
+})
+
+
+@Injectable()
+export  class SlideShowComponent implements OnInit {
+
+  constructor() { }
+
+
+
+  ngOnInit(): void {
+    if(this.autoSlide){
+      this.autoSlideImages();
+    }
+  }
+  @Input() autoSlide = false
+  @Input() slideInterval=3000;
+
+@Input()images: carouselImage[] = [
+{
+  imageSrc:"./assets/img/pic1.jpg",
+  imageAlt:"t",
+},
+{
+  imageSrc:"./assets/img/pic2.jpg",
+  imageAlt:"t",
+},
+]
+ 
+  slides: string [] = ['./assets/img/pic1.jpg', './assets/img/pic2.jpg']
+  selectedIndex=0;
+
+  autoSlideImages():void{
+    setInterval(()=>{
+      this.onNextClick();},this.slideInterval);
+      
+    
+  }
+
+  getSlide() {
+      return this.slides[this.selectedIndex];
+  }
+
+  onPrevClikc():void{
+    if(this.selectedIndex ===0){
+      this.selectedIndex = this.images.length -1;
+    }else{
+      this.selectedIndex --;
+    }
+  }
+
+  onNextClick():void{
+    if(this.selectedIndex ===this.images.length-1){
+      this.selectedIndex =0;
+    }else{
+      this.selectedIndex ++;
+    }
+  }
+  }
+
+
