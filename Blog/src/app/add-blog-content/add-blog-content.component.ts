938f1264d1909ebd3157826afa78706a464587b1
roxy22r <raksana280@gmail.com>
2022-08-27 09:57:22 +0200

    RAK New Blog Content added loading add pargraph

--- Blog/src/app/add-blog-content/add-blog-content.component.ts
+++ Blog/src/app/add-blog-content/add-blog-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { from, fromEventPattern } from 'rxjs';
 
 @Component({
   selector: 'app-add-blog-content',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-blog-content.component.css']
 })
 export class AddBlogContentComponent implements OnInit {
-
+  public loading = new Array(4);
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  
+  onClick(): void {
+    const node = document.createElement("div");
+
+    let Mform = <HTMLElement>document.getElementById("addNewPargraph");
+
+
+    node.innerHTML =
+    "<label class='form-label'>Blog Title:</label>"+
+    "<input type='text' class='form-control'/><br>"+
+    "<label class='form-label'>Blog Content:</label>"+
+    "<textarea class='form-control'></textarea><br>"+
+    "<br><label class='form-label'>Blog Picture:</label>"+
+    "<br><input type='file'   class='form-control'id='blogPic' name='filename'><br>";
+    Mform.appendChild(node);
+}
 }
