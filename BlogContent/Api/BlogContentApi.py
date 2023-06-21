91d5362d1a78946559fe5403f18c667361336a1c
roxy22r <raksana280@gmail.com>
2023-06-21 14:16:35 +0200

    Removed useless direcotry

--- BlogContent/Api/BlogContentApi.py
+++ BlogContent/Api/BlogContentApi.py
@@ -7,15 +7,16 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 app.config["DEBUG"] = True
 
 
-blogItems = [{'id': '1', 'title': 'Learn Python', 'content': 'someeeeText asdfa  s'},
-             {'id': '2', 'title': 'Sky Python', 'content': 'someeeeText asdfa  s'},
-             {'id': '3', 'title': 'fire Python', 'content': 'someeeeText asdfa  s'},
-             {'id': '4', 'title': 'deep Python', 'content': 'someeeeText asdfa  s'}]
+blogItems = [{'id': '1', 'title': 'Learn Python', 'content': 'someeeeText asdfa Learn  s'},
+             {'id': '2', 'title': 'Sky Python', 'content': 'someeeeText asdfa  s Sky'},
+             {'id': '3', 'title': 'fire Python', 'content': 'someeeeText asdfa  s fire '},
+             {'id': '4', 'title': 'deep Python', 'content': 'someeeeText asdfa  s deep'}]
 
 
 @app.route("/Blog/contents", methods=['GET'])
 def getBlogContent():
     return jsonify(blogItems)
+
 
 
 app.run()
